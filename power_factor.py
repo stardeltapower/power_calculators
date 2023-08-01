8af325eb6d1927ec933109a3d1da86d53e4782db
stardeltapower <65967874+stardeltapower@users.noreply.github.com>
2023-08-01 23:47:09 +0200

    Update power_factor.py
    Updated based on ChatGTP suggestions for using appropriate exceptions, meaningful variable names, reducing redundant calculations, and more precise comments.

--- power_factor.py
+++ power_factor.py
@@ -1,57 +1,49 @@
-'''
-Simple power factor calculator.
-
-a and b can be any of ['s', 'p', 'q', ['pf', 'angle']]
-a_value and b_value correspond to selected a and b variables
-
-returns dictonary including results ['s', 'p', 'q', 'pf', 'angle']
-'''
-
 import math
 
-def power_factor(a, a_value, b, b_value, rounding=4):
-	values = {}
-	values['input_a'] = a
-	values['input_b'] = b
-	values[a] = a_value
-	values[b] = b_value
-	
-	if 'pf' in values or 'angle' in values:
-		if 'angle' in values:
-			values['pf'] = math.cos(math.radians(values['angle']))
-		else:
-			values['angle'] = math.degrees(math.acos(values['pf']))
+def calculate_power_values(input_var_1, value_1, input_var_2, value_2, decimal_places=4):
+    """
+    Calculate power factor, apparent power, active power, reactive power, and angle given any two of them.
 
-		if 's' in values and 'pf' in values:
-			values['p'] = values['s'] * values['pf']
-			values['q'] = math.sqrt(values['s']**2 - values['p']**2)
-		elif 'p' in values and 'pf' in values:
-			values['s'] = values['p'] / values['pf']
-			values['q'] = math.sqrt(values['s']**2 - values['p']**2)
-		elif 'q' in values and 'pf' in values:
-			if values['pf'] == 0:
-				values['s'] = values['q']
-				values['p'] = 0
-			else:
-				values['s'] = values['q'] / math.sin(math.radians(values['angle']))
-				values['p'] = math.sqrt(values['s']**2 - values['q']**2)
-		else:
-			return False
-	else:
-		if 's' in values and 'p' in values:
-			values['q'] = math.sqrt(values['s']**2 - values['p']**2)
-		elif 's' in values and 'q' in values:
-			values['p'] = math.sqrt(values['s']**2 - values['q']**2)
-		elif 'p' in values and 'q' in values:
-			values['s'] = math.sqrt(values['p']**2 + values['q']**2)
-		else:
-			return False
-		
-		values['pf'] = values['p'] / values['s']
-		values['angle'] = math.degrees(math.acos(values['pf']))
-	
-	for key, value in values.items():
-		if isinstance(value, float):
-			values[key] = round(value, rounding)
+    Parameters:
+    input_var_1, input_var_2: Variables to be calculated. Can be 's', 'p', 'q', 'pf', 'angle'
+    value_1, value_2: Corresponding values of input_var_1 and input_var_2.
+    decimal_places: Number of decimal places to round the results to.
 
-	return values
+    Returns:
+    Dictionary with calculated values of 's', 'p', 'q', 'pf', 'angle'
+    """
+    power_values = {input_var_1: value_1, input_var_2: value_2}
+
+    # Calculate power factor and angle if they are not provided
+    if 'pf' not in power_values and 'angle' not in power_values:
+        if 's' not in power_values or 'p' not in power_values:
+            raise ValueError("Cannot calculate power factor and angle without 's' and 'p' values")
+
+        power_values['pf'] = power_values['p'] / power_values['s']
+        power_values['angle'] = math.degrees(math.acos(power_values['pf']))
+
+    # Calculate power factor or angle if one of them is not provided
+    elif 'pf' not in power_values:
+        power_values['pf'] = math.cos(math.radians(power_values['angle']))
+    elif 'angle' not in power_values:
+        power_values['angle'] = math.degrees(math.acos(power_values['pf']))
+
+    # Calculate 's', 'p', and 'q' values
+    if 's' not in power_values:
+        if 'p' in power_values and 'q' in power_values:
+            power_values['s'] = math.sqrt(power_values['p']**2 + power_values['q']**2)
+        else:
+            raise ValueError("Cannot calculate 's' value without 'p' and 'q' values")
+
+    if 'p' not in power_values:
+        power_values['p'] = power_values['s'] * power_values['pf']
+
+    if 'q' not in power_values:
+        power_values['q'] = math.sqrt(power_values['s']**2 - power_values['p']**2)
+
+    # Round results to the specified number of decimal places
+    for key, value in power_values.items():
+        if isinstance(value, float):
+            power_values[key] = round(value, decimal_places)
+
+    return power_values
